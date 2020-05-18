174530e62e6f12bb3d9d0c96022188e9d5974898
manpenaloza <manuel.penaloza@gmx.at>
2020-05-18 09:20:01 +0200

    fix blog post social sidebar

--- src/blog/components/hero.js
+++ src/blog/components/hero.js
@@ -5,11 +5,11 @@ import "tachyons";
 export default (props) => {
   const date = new Date(props.date);
   return (
-    <div className="bg-violett text-near-white px-16 py-20 leading-tight flex flex-column justify-center items-center">
+    <div className="bg-violett text-near-white px-16 py-20 leading-tight flex flex-column justify-center items-center text-center">
       <span className="font-sans-serif uppercase text-sm">
         {props.category}
       </span>
-      <h1 className="text-center text-2xl md:text-4xl lg:text-6xl my-4 lg:my-8">{props.title}</h1>
+      <h1 className="text-2xl md:text-4xl lg:text-6xl my-4 lg:my-8">{props.title}</h1>
       <span className="sans-serif tracked ttu f6 mb2">by {props.author}</span>
       <span className="sans-serif tracked ttu f6">
         {date.toLocaleDateString("de-DE")}
--- src/blog/components/sidebar.js
+++ src/blog/components/sidebar.js
@@ -1,45 +1,41 @@
-import React from 'react';
-import {
-  FaPinterestP,
-  FaFacebookF
-} from 'react-icons/fa';
-import {FiMail} from 'react-icons/fi'
-import { StaticQuery, graphql } from 'gatsby';
-import 'tachyons';
+import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+import { graphql, useStaticQuery } from "gatsby";
 
+export default ({ location }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          siteUrl
+        }
+      }
+    }
+  `);
 
-export default (props) => {
-  let { desc, img, location } = props;
-  location = encodeURIComponent(location.pathname);
-  desc = encodeURIComponent(desc);
-  img = encodeURIComponent(img);
+  const base = encodeURIComponent(data.site.siteMetadata.siteUrl);
+  const path = encodeURIComponent(location);
+
   return (
-    <StaticQuery
-      query={graphql`
-        query {
-          site {
-            siteMetadata {
-              siteUrl
-            }
-          }
-        }  
-      `}
-      render={data => {
-        const base = encodeURIComponent(data.site.siteMetadata.siteUrl);
-        return (
-        <div className="dn db-l" style={{gridArea: "sidebar"}}>
-          <div className="w3 bg-dark-gray flex flex-wrap" style={{position: "sticky", top: "3.5rem"}}>
-            <a
-              href={`https://pinterest.com/pin/create/button/?url=${base + location}&description=${desc}&media=${base + img}`}
-              className="w-100 h3 flex items-center justify-center b near-white"><FaPinterestP /></a>
-            <a
-              href={`https://www.facebook.com/sharer/sharer.php?u=${base + location}`}
-              className="w-100 h3 flex items-center justify-center b near-white"><FaFacebookF /></a>
-            <a
-              href={`mailto:?&body=${base + location}`}
-              className="w-100 h3 flex items-center justify-center b near-white"><FiMail /></a>
-          </div>
-        </div>
-    )}} />
-  )
-}
+    <div className="hidden md:block" style={{ gridArea: "sidebar" }}>
+      <div
+        className="w-16 bg-dark-gray flex flex-wrap"
+        style={{ position: "sticky", top: "3.5rem" }}
+      >
+        <a
+          href={`https://www.facebook.com/sharer/sharer.php?u=${base + path}`}
+          className="w-100 h-16 flex items-center justify-center font-bold text-near-white"
+        >
+          <FaFacebookF />
+        </a>
+        <a
+          href={`mailto:?&body=${base + path}`}
+          className="w-100 h-16 flex items-center justify-center font-bold text-near-white"
+        >
+          <FiMail />
+        </a>
+      </div>
+    </div>
+  );
+};
