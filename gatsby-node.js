56a2a80fadfa865163faafcb455d8137145c4033
manpenaloza <manuel.penaloza@gmx.at>
2020-06-09 10:55:44 +0200

    create blog post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,17 +44,19 @@ exports.createPages = ({ graphql, actions }) => {
         if (result.errors) {
           reject(result.errors);
         }
-        const allPosts = result.data.allPosts.edges;
+        // const allPosts = result.data.allPosts.edges;
 
         // next line: new code - don't delete
         const allBlogPosts = result.data.allBlogPosts.edges;
 
-        const groupedPosts = result.data.allPosts.group;
+        // const groupedPosts = result.data.allPosts.group;
         const paginationTemplate = path.resolve("src/blog/index.js");
-        const postsPerPage = 10;
-        let numPages = Math.ceil(allPosts.length / postsPerPage);
+        const postsPerPage = 5;
+        let numPages = Math.ceil(allBlogPosts.length / postsPerPage);
 
         // Creating the main blog index
+        // code locig secures it's as many 
+        // loop iterations as numPages for blog overview pages exist
         Array.from({ length: numPages }).forEach((_, i) => {
           createPage({
             path: i === 0 ? "/blog" : `/blog/${i + 1}`,
@@ -68,28 +70,28 @@ exports.createPages = ({ graphql, actions }) => {
           });
         });
 
-        // Creating all category pages.
-        let category;
-        let categoryPosts;
-        const categoryTemplate = path.resolve("src/blog/category.js");
-        groupedPosts.forEach((group, _) => {
-          category = group.fieldValue;
-          categoryPosts = group.edges;
-          numPages = Math.ceil(categoryPosts.length / postsPerPage);
-          Array.from({ length: numPages }).forEach((_, i) => {
-            createPage({
-              path: i === 0 ? `/${category}` : `/${category}/${i + 1}`,
-              component: categoryTemplate,
-              context: {
-                limit: postsPerPage,
-                skip: i * postsPerPage,
-                nextPage: `/${category}/${i + 2}`,
-                pageNumber: i + 1,
-                category: category,
-              },
-            });
-          });
-        });
+        // CATEGORY PAGES NOT NEEDED - Creating all category pages.
+        // let category;
+        // let categoryPosts;
+        // const categoryTemplate = path.resolve("src/blog/category.js");
+        // groupedPosts.forEach((group, _) => {
+        //   category = group.fieldValue;
+        //   categoryPosts = group.edges;
+        //   numPages = Math.ceil(categoryPosts.length / postsPerPage);
+        //   Array.from({ length: numPages }).forEach((_, i) => {
+        //     createPage({
+        //       path: i === 0 ? `/${category}` : `/${category}/${i + 1}`,
+        //       component: categoryTemplate,
+        //       context: {
+        //         limit: postsPerPage,
+        //         skip: i * postsPerPage,
+        //         nextPage: `/${category}/${i + 2}`,
+        //         pageNumber: i + 1,
+        //         category: category,
+        //       },
+        //     });
+        //   });
+        // });
 
         // nexb block - new code: don't delete
         const blogPostTemplate = path.resolve("src/blog/blogPostTemplate.js");
--- src/blog/index.js
+++ src/blog/index.js
@@ -1,75 +1,75 @@
-import React from 'react';
-import Layout from '../common/layouts';
-import { Link, graphql } from 'gatsby';
-import Breadcrumbs from './components/breadcrumbs';
-import Preview from './components/postPreview.js';
-import Seo from '../common/seo';
-import 'tachyons';
+import React from "react";
+import Layout from "../common/layouts";
+import { Link, graphql } from "gatsby";
+import Breadcrumbs from "./components/breadcrumbs";
+import MultiLink from "gatsby-universal-link"
+import Preview from "./components/postPreview.js";
+import Seo from "../common/seo";
 
+export default function BlogIndex({ data }, context) {
+  const posts = data.posts.edges;
+  const hasNext = data.posts.pageInfo.hasNextPage;
 
-export default class BlogIndex extends React.Component {
-  render() {
-    const posts = this.props.data.posts.edges;
-    const hasNext = this.props.data.posts.pageInfo.hasNextPage;
-    return (
-      <Layout>
-        <Seo
-          title={`All Blog Posts - Page ${this.props.pageContext.pageNumber}`}
-          description={`Index of all blog posts. Page ${this.props.pageContext.pageNumber}`}
+  return (
+    <Layout>
+      <Seo
+        title={`Alle Blog Beiträge - Seite ${context.pageNumber}`}
+        description={`Übersicht aller Blog Beiträge. Seite  ${context.pageNumber}`}
+      />
+      <div className="pv5 flex items-center justify-center bg-washed-red">
+        <h1 className="fw1 tc f2 display">Alle Blog Beiträge</h1>
+      </div>
+      <div className="mw9 center">
+        <Breadcrumbs
+          lastName="Blog"
+          lastPath="/blog"
+          currentPage={`Page ${context.pageNumber}`}
         />
-        <div className="pv5 flex items-center justify-center bg-washed-red">
-          <h1 className="fw1 tc f2 display">All Blog Posts</h1>
+        {posts.map(({ node }) => (
+          <div>{node.title}</div>
+          // <Preview
+          //   fluidImage={node.frontmatter.postImage.childImageSharp.fluid}
+          //   slug={node.frontmatter.slug}
+          //   title={node.frontmatter.title}
+          //   date={node.frontmatter.date}
+          //   category={node.frontmatter.category}
+          //   description={node.frontmatter.metaDescription}
+          // />
+        ))}
+        <div className="pv5 flex w-100">
+          {hasNext && (
+            <MultiLink
+              className="dark-gray sans-serif ttu tracked no-underline"
+              to={context.nextPage}
+            >
+              Nächste Seite &rarr;
+            </MultiLink>
+          )}
         </div>
-        <div className="mw9 center">
-          <Breadcrumbs
-            lastName="Blog"
-            lastPath="/blog"
-            currentPage={`Page ${this.props.pageContext.pageNumber}`} />
-          {posts.map(({node}) => (
-            <Preview
-              fluidImage={node.frontmatter.postImage.childImageSharp.fluid}
-              slug={node.frontmatter.slug}
-              title={node.frontmatter.title}
-              date={node.frontmatter.date}
-              category={node.frontmatter.category}
-              description={node.frontmatter.metaDescription} />
-          ))}
-          <div className="pv5 flex w-100">
-            {hasNext &&
-                <Link
-                  className="dark-gray sans-serif ttu tracked no-underline"
-                  to={this.props.pageContext.nextPage}>Next Page &rarr;</Link>
-            }
-          </div>
-        </div>
-      </Layout>
-    )
-  }
+      </div>
+    </Layout>
+  );
 }
-
 
 export const blogListQuery = graphql`
   query posts($skip: Int!, $limit: Int!) {
-    posts: allMarkdownRemark(
-      filter: {frontmatter: {type: {eq: "post"}}},
-      sort: {fields: frontmatter___date, order: DESC},
-      limit: $limit,
-      skip: $skip,
-    ) {
+    posts: allContentfulBlog(limit: $limit, skip: $skip) {
       edges {
         node {
-          frontmatter {
-            title
-            date(formatString: "MMM Do YYYY")
-            category
-            metaDescription
-            slug
-            postImage {
-              childImageSharp {
-                fluid(maxWidth: 1080, maxHeight: 512) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
+          id
+          title
+          slug
+          childContentfulBlogBodyRichTextNode {
+            json
+          }
+          introImage {
+            fluid(maxWidth: 200) {
+              aspectRatio
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+              sizes
             }
           }
         }
@@ -79,4 +79,4 @@ export const blogListQuery = graphql`
       }
     }
   }
-`
+`;
