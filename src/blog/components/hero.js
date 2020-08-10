5942ea4fda3bc360ffc2a219198d85e62801c57d
manpenaloza <manuel.penaloza@gmx.at>
2020-08-10 11:53:02 +0200

    bump versions + uppercase text

--- src/blog/components/hero.js
+++ src/blog/components/hero.js
@@ -10,7 +10,7 @@ export default (props) => {
         {props.category}
       </span>
       <h1 className="text-2xl md:text-4xl lg:text-6xl my-4 lg:my-8">{props.title}</h1>
-      <span className="sans-serif tracked ttu f6 mb2">verfasst von<br />{props.author}</span>
+      <span className="sans-serif tracked f6 mb2">verfasst von<br />{props.author.toUpperCase()}</span>
       {/* <span className="sans-serif tracked ttu f6">
         {date.toLocaleDateString("de-DE")}
       </span> */}
